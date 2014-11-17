4dd040d41e8b52d261cd90b56e94da3132b6762d
Tomasz Werbicki <tomasz@werbicki.net>
2014-11-17 19:06:39 +0100

    initial commit

---
+++ core.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 10:34:21 Sep 9, 2013 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:17:42 Nov 17, 2014.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+function start() {
+	var accordion = new Accordion('#offer h3', '#offer .content', { opacity: false , alwaysHide: true});
+}
+
+window.addEvent('load', start);
